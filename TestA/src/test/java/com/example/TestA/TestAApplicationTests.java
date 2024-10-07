3365d41c79b4b033d5b6755616f8cc15692849a8
alexgrent <51237839+alexgrent@users.noreply.github.com>
2024-10-07 13:45:14 +0200

    Add files via upload

---
+++ TestA/src/test/java/com/example/TestA/TestAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
